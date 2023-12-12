d5c48cf5c8ff9ac5c36b6470bbdf263d01366ef5
Blacksquad007 <112714180+Blacksquad007@users.noreply.github.com>
2023-12-13 02:00:33 +0530

    login/signup

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Loginpage from './Component/Loginpage';
+import SignupForm from './Component/SignupForm';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('SignupForm');
+
+  const handleNavigate = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {currentPage === 'SignupForm' && <SignupForm onNavigate={() => handleNavigate('Loginpage')} />}
+      {currentPage === 'Loginpage' && <Loginpage onNavigate={() => handleNavigate('SignupForm')} />}
     </div>
   );
 }
