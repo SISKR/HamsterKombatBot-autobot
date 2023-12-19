a1ef01bcf2f546bf3f2af6f5705faa3a24f6ed1c
Blacksquad007 <112714180+Blacksquad007@users.noreply.github.com>
2023-12-19 10:58:25 +0530

    nav bar

---
+++ react-project/src/Component/Text.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function card() {
+  return (
+    <div>
+      <h2>Transform Your Space: Elevate Your Home with Our Expert Interior Renovation Designs.</h2>
+      <h2>Discover the Art of Redefining Interiors</h2>
+    </div>
+  )
+}
